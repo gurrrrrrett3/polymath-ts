4eb6b62047624efbc5453fbea973f3d26132fe4a
Garrett Keneau <gart@gart.sh>
2024-03-18 09:36:18 -0400

    remove debug

--- src/index.ts
+++ src/index.ts
@@ -29,8 +29,6 @@ app.post("/upload", async (req, res) => {
 
     // id is formencoded 
 
-    console.log(`debug: upload request made: ${JSON.stringify(req.body)}`)
-
     const id = req.body.id as string;
     const pack = req.files?.pack as fileUpload.UploadedFile;
 
@@ -44,9 +42,6 @@ app.post("/upload", async (req, res) => {
 
     const hash = packManager.register(pack.data, id, (req.headers["x-forwarded-for"] || req.headers["x-real-ip"] || req.socket.remoteAddress) as string);
     const url = `${config.serverUrl}/pack.zip?id=${hash}`
-    
-    console.log(`Pack ${hash} uploaded by ${req.headers["x-forwarded-for"] || req.headers["x-real-ip"] || req.socket.remoteAddress} with id ${id}`)
-    console.log(`Response: ${JSON.stringify({url, sha1: hash})}`)
     
     res.status(200).json({
         url,
@@ -65,8 +60,6 @@ app.post("/upload", async (req, res) => {
 app.get("/pack.zip", (req, res) => {
     const id = req.query.id as string;
 
-    console.log(`debug: pack.zip request made: ${JSON.stringify(req.query)}`)
-
     if (!id) {
         return res.status(400).json({"error": "No id provided"})
     }
@@ -76,8 +69,6 @@ app.get("/pack.zip", (req, res) => {
     if (!pack) {
         return res.status(404).json({"error": "No pack found"})
     }
-
-    console.log(`Pack ${id} requested by ${req.headers["x-forwarded-for"] || req.headers["x-real-ip"] || req.socket.remoteAddress}`)
 
     res.setHeader('Content-type', 'application/zip').sendFile(pack);
 })
